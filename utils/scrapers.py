cd4f723eb56a32d3f75cee358d5eea9c22d537f1
Ifelaj <ifelaj93@yahoo.co.uk>
2026-02-24 16:32:34 +0100

    Create scrapers.py

---
+++ utils/scrapers.py
@@ -0,0 +1,5 @@
+def get_competitor_news(brand, region):
+    return [
+        {"title": f"{brand} launches new device in {region}"},
+        {"title": f"{brand} expands retail strategy in {region}"}
+    ]
