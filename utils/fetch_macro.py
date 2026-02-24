52c496e85daa129a1287bee111b0ba11e7956598
Ifelaj <ifelaj93@yahoo.co.uk>
2026-02-24 16:31:29 +0100

    Create fetch_macro.py

---
+++ utils/fetch_macro.py
@@ -0,0 +1,12 @@
+def get_macro_indicators_nigeria():
+    return {
+        "Inflation": "34%",
+        "FX Rate": "1450 NGN/USD",
+        "GDP Growth": "3.2%"
+    }
+
+def get_macro_indicators_kenya():
+    return {
+        "GDP Growth": "4.7%",
+        "Interest Rate": "10.75%"
+    }
